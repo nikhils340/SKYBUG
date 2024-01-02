6eccbc0d90b457f4387588980deaf1165022f9ee
NIKHIL SUNIL SHRIRAO <121716756+nikhils340@users.noreply.github.com>
2024-01-02 23:19:36 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,51 @@
+let btns = document.querySelectorAll(".num-button");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelector("#result-box");
+let clearBtn = document.querySelector('#clear');
+let clearLastBtn = document.querySelector('#clearLast'); // Add this line to select the "clearLast" button
+
+let total = document.querySelector("#total");
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+
+btnSpread.forEach((button, i) => {
+    button.addEventListener("click", () => {
+
+        if (resultBox.innerHTML == "0") {
+            resultBox.innerHTML = "";
+        }
+
+        let value = btns[i].innerHTML;
+        resultBox.innerHTML += value;
+    });
+});
+
+function evaluate(fn) {
+    return new Function('return ' + fn)();
+}
+
+total.addEventListener('click', () => {
+    let allInputs = resultBox.innerHTML;
+
+    resultBox.innerHTML = evaluate(allInputs);
+
+    console.log(evaluate(allInputs));
+})
+
+clearBtn.addEventListener('click', () => {
+    resultBox.innerHTML = "0";
+})
+
+clearLastBtn.addEventListener('click', () => {
+    let currentResult = resultBox.innerHTML;
+    if (currentResult !== "0") {
+        // Remove the last character from the resultBox
+        resultBox.innerHTML = currentResult.slice(0, -1);
+        // currentResult=0;
+        if (resultBox.innerHTML === "") {
+            resultBox.innerHTML = "0";
+        }
+    }
+    
+});
